6a1c92d74000bfba560236aa612ed58060630213
Валерия Оглоблина <nalaray547@gmail.com>
2025-12-18 03:04:10 +0300

    ver 9.5

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,9 @@
 import { randomInt } from 'node:crypto'
 
-const isEven = (num) => num % 2 === 0
+const isEven = num => num % 2 === 0
 const generateRandomNumber = () => randomInt(1, 101)
 
-export default function Even() {
+export default function generateEvenRound() {
   const number = generateRandomNumber()
   const question = number
   const correctAnswer = isEven(number) ? 'yes' : 'no'
